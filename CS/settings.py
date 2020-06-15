12e66ef6d51cd6447bbb1769b9f6624fe08f9585
191229R <ricardomazamiranda@gmail.com>
2020-06-15 15:28:34 -0500

    add

--- CS/settings.py
+++ CS/settings.py
@@ -28,6 +28,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['clienteservidor12.herokuapp.com']
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -60,9 +62,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CS.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
@@ -131,6 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 try:
